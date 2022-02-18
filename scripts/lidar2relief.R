956456fc3de2c989b53d32698e429d2d3a3f2d62
jesus-davila <jesus.sanchez.davila0@gmail.com>
2022-02-18 09:51:02 +0100

    actualizacion

--- scripts/lidar2relief.R
+++ scripts/lidar2relief.R
@@ -2,20 +2,26 @@
 
 library(lidR)
 
-# descarga de los archivos laz
-las <- readLAScatalog("C:/Users/Jesus/Documents/lidar")
+#descargo los datos lidar
+las <- readLAScatalog("C:/Users/Jesus/Documents/paquilo/laz", progress = TRUE)
+
+#filtro datos innecesarios o erroneos
+opt_filter(las) <- "-drop_z_below 0 -drop_class 1 -drop_class 7 -drop_class 12"
 
 # calculo del MDT de la zona de estudio
-# primero guardar el archivo en disco
-opt_output_files(las) <- "C:/Users/Jesus/Documents/lidar/dtm_{XLEFT}_{YBOTTOM}"
+
+las@output_options$drivers$Raster$param$overwrite <- TRUE # esto es para sobreescribir los archivos, pero no funciona
+opt_output_files(las) <- "C:/Users/Jesus/Documents/lidar/dtm_{XLEFT}_{YBOTTOM}"# primero guardar el archivo en disco
 dtm <- grid_terrain(las, 1, tin()) # hacer el MDT con una resolucion de 1 metro
 plot(dtm)
 
-# hago la normalizacion de los datos
-rois@output_options$drivers$Raster$param$overwrite <- TRUE
-opt_output_files(las) <- "C:/Users/Jesus/Documents/lidar/norm_{XLEFT}_{YBOTTOM}"
-norm <- normalize_height(las_cat, dtm,na.rm = TRUE)
+# calculo la altura de los edificios y arboles
+opt_output_files(las) <- "C:/Users/Jesus/Documents/paquilo/chm_{XLEFT}_{YBOTTOM}"
+chm <- rasterize_canopy(las, res = 1, pitfree(c(0,10,20), c(0, 1)))
+plot(chm)
 
-plot(norm)
+#PARA VERLO EN 3D, pero da fallos
+library(rassta)
+plot3D(dtm)
 
 
