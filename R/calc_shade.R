65869fa4f6603391a44da515fcc7873a13dffc95
Pakillo <f.rodriguez.sanc@gmail.com>
2022-05-05 17:49:22 +0200

    draft calc_shade function

--- R/calc_shade.R
+++ R/calc_shade.R
@@ -1,8 +1,8 @@
-#' Title
+#' Calculate shades from elevation map
 #'
-#' @param heights
-#' @param sun.altitude
-#' @param sun.angle
+#' @param heights Height matrix, as calculated by [terra_to_matrix()].
+#' @param sun.altitude Sun altitude (angle from the horizon, in degrees)
+#' @param sun.angle Sun angle in degrees (zero degrees is North, increasing clockwise)
 #' @param ... further arguments to [rayshader::ray_shade()]
 #'
 #' @return
@@ -10,5 +10,10 @@
 #'
 #' @examples
 calc_shade <- function(heights = NULL, sun.altitude = NULL, sun.angle = NULL, ...) {
+
+  shademap <- rayshader::ray_shade(heightmap = heights,
+                                   sunaltitude = sun.altitude,
+                                   sunangle = sun.angle,
+                                   ...)
 
 }
