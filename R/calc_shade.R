57d1d98cb64cdf3b1913f3fed59ca596495afde4
Pakillo <f.rodriguez.sanc@gmail.com>
2022-05-06 09:40:38 +0200

    calc_shade: return SpatRaster rather than matrix

--- R/calc_shade.R
+++ R/calc_shade.R
@@ -3,17 +3,27 @@
 #' @param heights Height matrix, as calculated by [terra_to_matrix()].
 #' @param sun.altitude Sun altitude (angle from the horizon, in degrees)
 #' @param sun.angle Sun angle in degrees (zero degrees is North, increasing clockwise)
+#' @param lambert Logical
+#' @param crs coordinate reference system, see [terra::crs()]
 #' @param ... further arguments to [rayshader::ray_shade()]
 #'
-#' @return
+#' @return A SpatRaster of light intensities, as produced by [rayshader::ray_shade()]
 #' @export
 #'
-#' @examples
-calc_shade <- function(heights = NULL, sun.altitude = NULL, sun.angle = NULL, ...) {
 
-  shademap <- rayshader::ray_shade(heightmap = heights,
+calc_shade <- function(heights = NULL,
+                       sun.altitude = NULL, sun.angle = NULL,
+                       lambert = TRUE,
+                       crs = "", ...) {
+
+   # Need to vectorize for sun altitude and angle
+
+  shademat <- rayshader::ray_shade(heightmap = heights,
                                    sunaltitude = sun.altitude,
                                    sunangle = sun.angle,
+                                   lambert = lambert,  # correct?
                                    ...)
+
+  shaderas <- matrix_to_terra(shademat, crs)
 
 }
