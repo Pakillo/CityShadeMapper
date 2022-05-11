61be8b648f3a7ad27512b31c2938098402b79680
Pakillo <f.rodriguez.sanc@gmail.com>
2022-05-11 18:47:00 +0200

    vectorize get_sun_position for several dates and times

--- R/get_sun_position.R
+++ R/get_sun_position.R
@@ -4,8 +4,10 @@
 #'
 #' @param lon Longitude (numeric value between -180 and 180)
 #' @param lat Latitude (numeric value between -90 and 90)
-#' @param date A character giving the date (e.g. "2021-02-19")
-#' @param hour Hour of the day. Integer number between 0 and 23, both included.
+#' @param date A Date object or character giving the date in YYYY-MM-DD format (e.g. "2021-02-19").
+#' Can be a vector too, e.g. c("2021-02-19", "2021-08-04").
+#' Note different years have no effect on sun position calculations.
+#' @param hour Hour of the day. Integer number (or numeric vector) between 0 and 23 (both included).
 #'
 #' @return A numeric matrix as returned by [solartime::computeSunPositionDoyHour()],
 #' but converted to degrees rather than radians.
@@ -13,10 +15,14 @@
 #'
 #' @examples
 #' sunpos <- get_sun_position(lon = -5.99, lat = 37.39, date = "2021-02-19", hour = 15)
+#' sunpos <- get_sun_position(lon = -5.99, lat = 37.39,
+#' date = c("2021-02-19", "2022-08-05"), hour = 10:14)
 get_sun_position <- function(lon = NULL, lat = NULL, date = NULL, hour = NULL) {
 
-  stopifnot(hour %in% seq(from = 0, to = 23, by = 1))
+  stopifnot(is.numeric(lon), lon > -180, lon < 180)
   stopifnot(is.numeric(lat), lat > -90, lat < 90)
+  stopifnot(hour %in% seq(from = 0, to = 23, by = 1))
+
 
   # Find time zone from lon-lat
   tz <- lutz::tz_lookup_coords(lat = lat, lon = lon, method = "fast", warn = FALSE)
@@ -24,9 +30,22 @@ get_sun_position <- function(lon = NULL, lat = NULL, date = NULL, hour = NULL) {
   # Get time difference with UTC
   time.offset <- lutz::tz_offset(date, tz = tz)$utc_offset_h
 
+  # Prepare dates and times
+  if (length(date) > 1 | length(hour) > 1) {
+
+    dates.hours <- expand.grid(date = date, hour = hour, stringsAsFactors = FALSE) |>
+      dplyr::arrange(date, hour)
+
+  } else {
+
+    dates.hours <- data.frame(date = date, hour = hour)
+
+  }
+
+  # Calculate sun position for each date and time
   sunpos.rad <- solartime::computeSunPositionDoyHour(
-    doy = lubridate::yday(date),  # alternatively, as.numeric(strftime(date, format = "%j"))
-    hour = hour,
+    doy = lubridate::yday(dates.hours$date),   # alternatively, as.numeric(strftime(date, format = "%j"))
+    hour = dates.hours$hour,
     latDeg = lat,
     longDeg = lon,
     timeZone = time.offset,
@@ -34,9 +53,11 @@ get_sun_position <- function(lon = NULL, lat = NULL, date = NULL, hour = NULL) {
   )
 
   sunpos.deg <- apply(subset(sunpos.rad,
-                             select = c("declination", "elevation", "azimuth")),
+                             select = c("elevation", "azimuth")),
                   MARGIN = c(1, 2),
                   FUN = rad2deg)
+
+  sunpos.deg <- cbind(dates.hours, hour.solar = sunpos.rad[, "hour"], sunpos.deg)
 
   sunpos.deg
 
