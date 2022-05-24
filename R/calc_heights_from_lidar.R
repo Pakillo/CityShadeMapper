2cb04be4aeae5846d193b0a415b1246dc2dbf9f9
jesus-davila <jesus.sanchez.davila0@gmail.com>
2022-05-24 18:48:37 +0200

    Add function to calculate height from LiDAR

--- R/calc_heights_from_lidar.R
+++ R/calc_heights_from_lidar.R
@@ -1,11 +1,32 @@
-#' Title
+#' Calculate height LiDAR
 #'
-#' @param variables
-#'
-#' @return
+#' @param las Lidar data
+#' @param res
+#' @param thresholds
+#' @param ... further arguments to [lidR::pitfree()]
+#' @return SpatRaster object with height LiDAR data
 #' @export
 #'
 #' @examples
-calc_heights_from_lidar <- function(variables) {
+#'
+#'
+calc_heights_from_lidar <- function(las = NULL,
+                                    res = 1,
+                                    thresholds = c(0,2,5,10,20),
+                                    ...
+) {
+  stopifnot(class(las) == "LAS" | class(las) == "LAScatalog")
+  stopifnot(is.numeric(res) | class(res) == "SpatRaster" )
+  if (is.numeric(res)) {
+    stopifnot(res > 0)
+  }
+  stopifnot(is.numeric(thresholds), length(thresholds) > 1)
+
+  dsm_height <- lidR::rasterize_canopy(las = las,
+                                       res = res,
+                                       lidR::pitfree(thresholds, ...))
 
 }
+
+
+
