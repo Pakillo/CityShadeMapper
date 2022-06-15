2388aede2aa13d0b91b4ee22d373744da54fe030
Pakillo <f.rodriguez.sanc@gmail.com>
2022-06-15 16:46:40 +0200

    add embrionary plot_shademap function

---
+++ R/plot_shademap.R
@@ -0,0 +1,31 @@
+#' Plot shade map
+#'
+#' @param shade.ras A [terra::SpatRaster()] object with shade intensity, as produced by [get_shade_raster()].
+#' @param legend Logical. Show legend?
+#' @param animate Logical. Show animation of all the `shade.ras` layers?
+#' @param ... Further arguments to [terra::plot()], or to [terra::animate()] if `animate` is `TRUE`.
+#'
+#' @return A static or animated plot.
+#' @export
+#'
+plot_shademap <- function(shade.ras = NULL, legend = TRUE, animate = FALSE, ...) {
+
+  if (animate) {
+
+    terra::animate(shade.ras,
+                   col = rev(colorRampPalette(RColorBrewer::brewer.pal(9, "Greys"))(20)),
+                   axes = FALSE,
+                   legend = legend,
+                   ...)
+
+  } else {
+
+    terra::plot(shade.ras,
+                col = rev(colorRampPalette(RColorBrewer::brewer.pal(9, "Greys"))(20)),
+                axes = FALSE,
+                legend = legend,
+                ...)
+
+  }
+
+}
