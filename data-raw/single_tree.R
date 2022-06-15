5adab82991ab456f9f05e96a50913074f631a204
Pakillo <f.rodriguez.sanc@gmail.com>
2022-06-15 14:14:47 +0200

    add example single_tree dataset #11

---
+++ data-raw/single_tree.R
@@ -0,0 +1,9 @@
+hmat <- matrix(data = rep(0, 9*9), nrow = 9, ncol = 9, byrow = TRUE)
+hmat[5,5] <- 4
+
+single_tree <- CityShadeMapper:::matrix_to_terra(hmat,
+                                                 crs = "epsg:25830",
+                                                 extent = c(230000, 230009, 4141250, 4141259))
+
+
+saveRDS(single_tree, file = "data/single_tree.rds")
