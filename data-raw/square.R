380ccf26f73800ea947a85bc71c819d7ff9e0907
Francisco Rodriguez-Sanchez <f.rodriguez.sanc@gmail.com>
2022-10-01 18:07:21 +0200

    add example datasets. Close #11

---
+++ data-raw/square.R
@@ -0,0 +1,17 @@
+hmat <- matrix(data = rep(0, 100*100), nrow = 100, ncol = 100, byrow = TRUE)
+
+#square
+hmat[20,20:80] <- 10
+hmat[80,20:80] <- 10
+hmat[20:80,20] <- 10
+hmat[20:80,80] <- 10
+
+# tree in the middle, 4 m high
+hmat[48:52, 48:52] <- 4
+
+square <- CityShadeMapper:::matrix_to_terra(hmat,
+                                            crs = "epsg:25830",
+                                            extent = c(230000, 230100, 4141250, 4141350))
+
+
+saveRDS(square, file = "data/square.rds")
