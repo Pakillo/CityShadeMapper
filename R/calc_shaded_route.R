cf36657bb7c016a55af85e289e241bf6eccf6810
Francisco Rodriguez-Sanchez <f.rodriguez.sanc@gmail.com>
2022-10-02 12:59:38 +0200

    document

--- R/calc_shaded_route.R
+++ R/calc_shaded_route.R
@@ -1,13 +1,11 @@
-#' Title
+#' Calculate shaded route
 #'
-#' @param origin
-#' @param destination
-#' @param date
-#' @param time
+#' @param origin origin coordinates.
+#' @param destination destination coordinates.
+#' @param date Character. Date in YYYY-MM-DD format.
+#' @param time Numeric. Hour in local time.
 #'
-#' @return
 #'
-#' @examples
 calc_shaded_route <- function(origin = NULL, destination = NULL, date = NULL, time = NULL) {
 
 }
