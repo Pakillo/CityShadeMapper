fab6d836ee130dddb8249e3033644917f485ab2f
Francisco Rodriguez-Sanchez <f.rodriguez.sanc@gmail.com>
2022-10-08 19:27:56 +0200

    read_lidar: select only "xyz" to save memory

--- R/read_lidar.R
+++ R/read_lidar.R
@@ -2,25 +2,30 @@
 #'
 #' @param folder Character. Path to folder containing las/laz files.
 #' Can also be a vector of file paths.
+#' @param select Character. Point attributes to read from the las/laz files.
+#' Use `select = "*"` to read all attributes. Default is "xyz" to save memory.
+#' See [lidR::readLAS()] for more details.
 #' @param filter Character. Optional. Use if you want to filter out some data points.
-#' For example, we could filter out noisy data points (class = 7).
+#' For example, we could filter out noisy data points (class = 7) using
+#' `filter = "-drop_class 7"`. Use `filter = ""` to read all data points.
 #' @param ... Further arguments for [lidR::readLAScatalog()].
 #'
 #' @return LAScatalog object with LiDAR data
 #' @export
 #' @examples
 #' \dontrun{
-#' lidar.file <- system.file("extdata", "PlazaNueva.laz",
-#' package = "CityShadeMapper", mustWork = TRUE)
-#' lidar <- read_lidar(lidar.file)
+#' las <- system.file("extdata", "PlazaNueva.laz", package = "CityShadeMapper")
+#' lidar <- read_lidar(las)
 #' }
 #'
 #'
 read_lidar <- function(folder = NULL,
+                       select = "xyz",
                        filter = "-drop_class 7",
                        ...) {
 
   lidR::readLAScatalog(folder = folder,
+                       select = select,
                        filter = filter,
                        ...)
 
