523c23e30a280a8418c8a29e2c574166777f0096
Francisco Rodriguez-Sanchez <f.rodriguez.sanc@gmail.com>
2022-10-08 22:50:55 +0200

    fill holes in cover classes raster

--- R/rasterize_lidar_cover_class.R
+++ R/rasterize_lidar_cover_class.R
@@ -1,38 +1,40 @@
-#' Rasterize cover classification of lidar points
+#' Get raster of cover classification from lidar points
 #'
-#' @param las.file Path to a LAS/LAZ object.
+#' @param las A [lidR::LAScatalog-class()] object, or a character vector with
+#' paths to LAS/LAZ objects.
 #' @param res Resolution of the resulting raster.
-#' @param group.classes Logical. Group classes in the output raster?
+#' @param fill.holes Logical. Try to fill holes in lidar point classification.
 #'
 #' @return A SpatRaster with the classification of cover types:
-#' 9 = water, 6 = buildings, 5 = vegetation > 3m, 4 = vegetation between 1 and 3 m high,
-#' 3 = vegetation < 1m, 2 = ground.
-#' Note that points classified as bridges (class 17) will be reclassified as ground.
-#' If group.classes is TRUE, the output SpatRaster will only have three values:
 #' 2 = ground (including low vegetation < 1m)
 #' 4 = high vegetation (> 1m)
 #' 6 = buildings
+#' 9 = water
 #' and NA values.
-#'
-#' @keywords internal
+#' Note that points classified as bridges (class 17) will be reclassified as ground.
+#' @export
 #'
 #' @examples
 #' \dontrun{
 #' pza <- system.file("extdata", "PlazaNueva.laz", package = "CityShadeMapper")
 #' pza.cover <- rasterize_lidar_cover_class(pza)
 #' }
-rasterize_lidar_cover_class <- function(las.file = NULL, res = 1, group.classes = TRUE) {
+rasterize_lidar_cover_class <- function(las = NULL, res = 1, fill.holes = TRUE) {
 
-  pts <- lidR::readLAS(las.file, select = "xyc", filter = "-keep_class 2 3 4 5 6 9 17")
+  pts <- lidR::readLAS(las, select = "xyc", filter = "-keep_class 2 3 4 5 6 9 17")
   #table(pts$Classification)
+  # see classes https://github.com/Pakillo/CityShadeMapper/issues/2#issuecomment-1117648511
   # reclassify bridges as ground
   pts$Classification[pts$Classification == 17] <- as.integer(2)
-  # see classes https://github.com/Pakillo/CityShadeMapper/issues/2#issuecomment-1117648511
+  # reclassify low vegetation (<1m) as ground
+  pts$Classification[pts$Classification == 3] <- as.integer(2)
+  # join classes 4 & 5 (vegetation > 1m high)
+  pts$Classification[pts$Classification == 5] <- as.integer(4)
 
   pts.class <- lidR::pixel_metrics(pts, func = max.class(Classification), res = res)
 
-  if (isTRUE(group.classes)) {
-    pts.class <- reclassify_cover(pts.class)
+  if (isTRUE(fill.holes)) {
+    pts.class <- fill_holes(pts.class)
   }
 
   pts.class
@@ -40,11 +42,12 @@ rasterize_lidar_cover_class <- function(las.file = NULL, res = 1, group.classes 
 }
 
 
-reclassify_cover <- function(ras = NULL) {
-  terra::subst(ras, from = c(3, 5), to = c(2, 4))
-  # Classify low vegetation < 1m (class 3) as ground (class 2)
-  # Unify high vegetation (> 1m) as value 4
-  # Value = 6 are buildings
-}
-
 max.class <- function(x) {max(x, na.rm = TRUE)}
+
+fill_holes <- function(ras = NULL) {
+
+  # try to fill holes (NA) in lidar classification of points
+  ras.agg <- terra::aggregate(ras, fact = 2, fun = "modal", na.rm = TRUE)
+  ras.disagg <- terra::disagg(ras.agg, fact = 2, method = "near")
+
+}
--- tests/testthat/test-rasterize_lidar_cover_class.R
+++ tests/testthat/test-rasterize_lidar_cover_class.R
@@ -1,9 +1,9 @@
 test_that("rasterize_lidar_cover_class works", {
   pza <- system.file("extdata", "PlazaNueva.laz", package = "CityShadeMapper")
-  pza.cover <- rasterize_lidar_cover_class(pza, res = 1, group.classes = TRUE)
+  pza.cover <- rasterize_lidar_cover_class(pza, res = 1)
 
   expect_equal(as.numeric(head(terra::values(pza.cover), n = 40)),
-               c(6, NA, 6, 6, 6, 6, NA, NA, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6,
-                 6, NA, 4, 2, NA, NA, NA, 6, 6, 6, 6, 6, 6, NA, 6, 6, 6, NA, 4,
-                 6, NA))
+               c(6, 6, 6, 6, 6, 6, NA, NA, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6,
+                 6, 2, 2, 2, 2, NA, NA, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6)
+  )
 })
