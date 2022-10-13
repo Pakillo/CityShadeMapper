68a7ce0367182327479607dd72efb4b8de9355d4
jesus-davila <jesus.sanchez.davila0@gmail.com>
2022-10-13 22:53:42 +0200

    update

--- R/calc_shaded_route.R
+++ R/calc_shaded_route.R
@@ -4,8 +4,43 @@
 #' @param destination destination coordinates.
 #' @param date Character. Date in YYYY-MM-DD format.
 #' @param time Numeric. Hour in local time.
+#' @param shadow Shadow raster map
 #'
 #'
-calc_shaded_route <- function(origin = NULL, destination = NULL, date = NULL, time = NULL) {
+
+calc_shaded_route <- function(origin = NULL, destination = NULL, date = NULL, time = NULL, shadow = NULL ) {
+
+  # matrix of cost
+  cost <- gdistance::transition(1/shadow=shadow,
+                        transitionFunction=mean,
+                        directions=8)
+
+  #geocorrection
+  cost_correction <- gdistance::geoCorrection(cost=cost, type = "c")
+
+  #calculate route
+  route <- gdistance::shortestPath(cost_correction=cost_correction, origin = origin, destination = destination,
+                                  output="SpatialLines")
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
