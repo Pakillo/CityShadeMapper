507a8da15456803a175e232298a6f836d810cc51
Francisco Rodriguez-Sanchez <f.rodriguez.sanc@gmail.com>
2022-10-16 00:41:13 +0200

    update save_cog to use GDAL's COG driver (internal fun)

--- R/save_cog.R
+++ R/save_cog.R
@@ -1,23 +1,22 @@
 #' Save raster as Cloud Optimised GeoTiff
 #'
-#' @param ras A shade raster as produced by [make_shademap()].
-#' @param filename Character. Output filename.
+#' Requires GDAL >= 3.1
+#'
+#' @param ras A SpatRaster (e.g. a shade raster as produced by [make_shademap()]).
+#' @param filename Character. Output filename. Must end with '.tif' extension.
 #'
 #' @return A cloud optimised geotiff saved on disk.
-#' @export
+#' @keywords internal
 #'
 save_cog <- function(ras = NULL, filename = NULL) {
 
   if (terra::crs(ras, describe = TRUE)$code != "4326") {
-    ras <- project_raster_geo(ras)
+    warning("This raster does not have EPSG:4326 projection.")
   }
 
   terra::writeRaster(ras,
                      filename = filename,
-                     filetype = "GTiff",
-                     datatype = "INT1U",
-                     gdal = c("TILED=YES",
-                              "COPY_SRC_OVERVIEWS=YES",
-                              "COMPRESS=DEFLATE"))
+                     filetype = "COG",
+                     datatype = "INT1U")
 
 }
