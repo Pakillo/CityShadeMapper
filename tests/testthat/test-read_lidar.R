3dd3566db26449350eabfc2945d0b2a4fd619df1
Francisco Rodriguez-Sanchez <f.rodriguez.sanc@gmail.com>
2024-03-25 14:24:26 +0100

    use terra::ext to get lidar extent

--- tests/testthat/test-read_lidar.R
+++ tests/testthat/test-read_lidar.R
@@ -2,13 +2,13 @@ test_that("read_lidar works", {
 
   lidar <- read_lidar(system.file("extdata", "PlazaNueva.laz",
                        package = "CityShadeMapper", mustWork = TRUE))
-  lidar.ext <- lidR::extent(lidar)
+  lidar.ext <- terra::ext(lidar)
 
   expect_true(inherits(lidar, "LAScatalog"))
-  expect_equal(lidar.ext@xmin, 234700)
-  expect_equal(round(lidar.ext@xmax), 234880)
-  expect_equal(lidar.ext@ymin, 4142100)
-  expect_equal(lidar.ext@ymax, 4142250)
+  expect_equal(terra::xmin(lidar.ext), 234700)
+  expect_equal(round(terra::xmax(lidar.ext)), 234880)
+  expect_equal(terra::ymin(lidar.ext), 4142100)
+  expect_equal(terra::ymax(lidar.ext), 4142250)
   expect_equal(lidR::st_crs(lidar)$input, "EPSG:3042")
 
 })
