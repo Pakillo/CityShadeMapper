2b84fdcc1ef08a8546eb95eb68c2516ed023e4dd
Francisco Rodriguez-Sanchez <f.rodriguez.sanc@gmail.com>
2024-03-25 14:53:41 +0100

    amend test

--- tests/testthat/test-rasterize_lidar_cover_class.R
+++ tests/testthat/test-rasterize_lidar_cover_class.R
@@ -3,7 +3,7 @@ test_that("rasterize_lidar_cover_class works", {
   pza.cover <- rasterize_lidar_cover_class(pza, res = 1)
 
   expect_equal(as.numeric(head(terra::values(pza.cover), n = 40)),
-               c(6, 6, 6, 6, 6, 6, NA, NA, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6,
+               c(6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6,
                  6, 2, 2, 2, 2, NA, NA, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6)
   )
 })
